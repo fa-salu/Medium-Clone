41a176471c1e42106265b35cf322c8ec061e1648
fa-salu <fasalurahman7515@gmail.com>
2025-01-10 21:17:39 +0530

    add image button functionality updated

--- client/src/components/new-post/addButton.tsx
+++ client/src/components/new-post/addButton.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import CodeIcon from "@mui/icons-material/Code";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { CircularProgress } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { uploadImage, resetImage } from "@/lib/features/uploadSlice";
+import { RootState } from "@/lib/store";
 
 export default function IconSet({
   showIcons,
@@ -16,18 +18,17 @@ export default function IconSet({
   storyRef: React.RefObject<HTMLDivElement>;
   setCoverImage: (url: string) => void;
 }) {
-  const [showImageUpload, setShowImageUpload] = useState(false);
   const dispatch = useAppDispatch();
-  const imageUrl = useAppSelector((state) => state.upload.url);
+  const { url, isLoading } = useAppSelector((state: RootState) => state.upload);
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
       dispatch(uploadImage(file))
         .unwrap()
-        .then(() => {
-          setShowImageUpload(false);
-        })
+        .then(() => {})
         .catch((error) => {
           console.error("Error uploading image:", error);
         });
@@ -35,14 +36,14 @@ export default function IconSet({
   };
 
   useEffect(() => {
-    if (imageUrl) {
-      setCoverImage(imageUrl);
+    if (url) {
+      setCoverImage(url);
 
       if (storyRef.current) {
         const editor = storyRef.current as HTMLDivElement;
 
         const imgElement = document.createElement("img");
-        imgElement.src = imageUrl;
+        imgElement.src = url;
         imgElement.alt = "uploaded-image";
 
         editor.appendChild(imgElement);
@@ -50,24 +51,36 @@ export default function IconSet({
 
       dispatch(resetImage());
     }
-  }, [imageUrl, storyRef, setCoverImage, dispatch]);
+  }, [url, storyRef, setCoverImage, dispatch]);
+
+  const handleIconClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
 
   return (
     <>
       {showIcons && (
         <div className="absolute right-40 top-20 flex space-x-2 bg-white shadow-md p-4">
-          <AddPhotoAlternateIcon onClick={() => setShowImageUpload(true)} />
-          {showImageUpload && (
-            <input
-              type="file"
-              onChange={handleImageUpload}
-              className="border p-2"
-            />
+          <AddPhotoAlternateIcon onClick={handleIconClick} />
+          <input
+            ref={fileInputRef}
+            type="file"
+            onChange={handleImageUpload}
+            className="hidden"
+          />
+
+          {isLoading ? (
+            <CircularProgress size={24} />
+          ) : (
+            <>
+              <VideoLibraryIcon />
+              <InsertDriveFileIcon />
+              <CodeIcon />
+              <MoreHorizIcon />
+            </>
           )}
-          <VideoLibraryIcon />
-          <InsertDriveFileIcon />
-          <CodeIcon />
-          <MoreHorizIcon />
         </div>
       )}
     </>
